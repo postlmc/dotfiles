c424b0e4c34dfec92460e1dc13b9d16774f712e7
Scott Thompson <69859+PostlMC@users.noreply.github.com>
2026-01-14 21:00:28 -0600

    available/enabled collection migrated
    This is the last big chunk of configuration files from the original dotfiles repo.

---
+++ available/homebrew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+command -v brew >/dev/null 2>&1 || return
+
+# Optimize Homebrew behavior for faster, cleaner operation
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_EMOJI=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+alias brew-up='${HOMEBREW_PREFIX}/bin/brew update && \
+        (${HOMEBREW_PREFIX}/bin/brew upgrade; ${HOMEBREW_PREFIX}/bin/brew upgrade --cask) && \
+        ${HOMEBREW_PREFIX}/bin/brew cleanup -s'
+alias brew86-up='${HOMEBREW86_PREFIX}/bin/brew86 update && \
+        (${HOMEBREW86_PREFIX}/bin/brew86 upgrade; ${HOMEBREW86_PREFIX}/bin/brew86 upgrade --cask) && \
+        ${HOMEBREW86_PREFIX}/bin/brew86 cleanup -s'
+
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
